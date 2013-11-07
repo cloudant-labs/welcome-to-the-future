913fa8856a7070e0f6ba430a8104278305463a50
Max Thayer <garbados@gmail.com>
2013-11-07 16:40:16 -0500

    initial sketch of frontend

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,51 @@
-var md = new Showdown.converter();
+angular.module('app', [])
+.constant('md', new Showdown.converter())
+.constant('pouch', new PouchDB('WttF'))
+.controller('HotCtrl', [])
+.controller('RecentCtrl', [])
+.controller('ProfileCtrl', [])
+.controller('SubmitCtrl', [])
+.controller('PostCtrl', [])
+.controller('LoginCtrl', [])
+.controller('SignupCtrl', [])
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl: 'index.html',
+    controller: 'HotCtrl'
+  })
+  .when('/recent', {
+    templateUrl: 'recent.html',
+    controller: 'RecentCtrl'
+  })
+  .when('/profile', {
+    templateUrl: 'profile.html',
+    controller: 'ProfileCtrl'
+  })
+  .when('/submit', {
+    templateUrl: 'submit.html',
+    controller: 'SubmitCtrl'
+  })
+  .when('/post/:id', {
+    templateUrl: 'post.html',
+    controller: 'PostCtrl'
+  })
+  .when('/login', {
+    templateUrl: 'login.html',
+    controller: 'LoginCtrl'
+  })
+  .when('/signup', {
+    templateUrl: 'signup.html',
+    controller: 'SignupCtrl'
+  })
 
-$(function(){
-  $.ajax({
-    url: '/readme',
-    success: function(data){
-      var sections = data.text.split(/(.*)#/g).filter(function(section){
-            return section && (section[0] !== '#');
-          }),
-          header = "#" + sections[0],
-          body = sections.slice(1).map(function(section){
-            return "##" + section;
-          }).join('\n');
-
-      $('#header').html(md.makeHtml(header));
-      $('#main').html(md.makeHtml(body));
-    }
+  .otherwise({
+    redirectTo: '/'
   });
-});
+}])
+// markdown filter for dynamic content
+.filter('markdown', ['md', function(md){
+  return function(input){
+    if(input) return md.makeHtml(input);
+  };
+}]);
